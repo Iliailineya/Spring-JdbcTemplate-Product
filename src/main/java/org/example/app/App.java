030d58edf33e0ac5465e4d041899ab88ea44a6b3
Iliailineya <ilavituk011@gmail.com>
2023-10-13 02:27:59 +0300

    Initial commit

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -3,7 +3,6 @@ package org.example.app;
 import org.example.app.utils.starter.AppStarter;
 
 public class App {
-
     public static void main(String[] args) {
         AppStarter.startApp();
     }
